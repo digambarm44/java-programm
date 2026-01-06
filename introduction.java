8c2692f022f9a11d6c34f479fe86f70b6f0870c3
digambarm44 <digambarm448@gmail.com>
2026-01-06 16:55:58 +0530

    first commit java programm

---
+++ introduction.java
@@ -0,0 +1,10 @@
+class introduction
+{
+public static void main(string[]args)
+{
+system.out.println("name:digambar mane");
+system.out.println("age:21");
+system.out.println("institute:qspiders");
+system.out.println("company:tata consultancy");
+}
+}
